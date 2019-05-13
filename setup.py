4001a277780c6757552a58ac0cc11b62af7b09c5
Gabriel Brammer <gbrammer@gmail.com>
2019-05-13 13:21:30 +0200

    switch to setuptools

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-from distutils.core import setup
-from distutils.extension import Extension
+# from distutils.core import setup
+# from distutils.extension import Extension
+from setuptools import setup
+from setuptools.extension import Extension
 
 import subprocess
 
