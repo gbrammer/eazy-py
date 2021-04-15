e1e11321e82fa7a3bfe6a5daea36a1abce3a9f17
cosmosLukas <lukaszalesky@lukass-MacBook-Pro.local>
2021-04-15 08:36:55 -1000

    added tests for user-specified zbest

--- eazy/tests/test_photoz.py
+++ eazy/tests/test_photoz.py
@@ -251,7 +251,13 @@ def test_sps_parameters():
                                      prior=True, beta_prior=True, simple=True)
     
     assert(np.allclose(z2['z_phot'], z_spec, rtol=1.e-2))
-    
+
+    # confirm that z2 has both 'z_pdf' and 'z_phot' columns and that they're different 
+    assert( np.any(z2['z_pdf'] != z2['z_phot']) )
+
+    # confirm that z2['z_pdf'] == zout['z_phot']
+    assert( np.all(z2['z_pdf'] == zout['z_phot']) )
+
     ### Check that sps parameters are different...
     
 def test_fit_stars():
