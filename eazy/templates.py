7dd5289eb9979acdbe950f52632ab1d280236dd6
Gabriel Brammer <gbrammer@gmail.com>
2021-05-14 15:31:42 +0200

    Add Redden example plot

--- eazy/templates.py
+++ eazy/templates.py
@@ -118,6 +118,37 @@ class Redden(object):
         Wrapper function for `dust_attenuation` and `dust_extinction` 
         reddening laws
 
+        .. plot::
+            :include-source:
+            
+            import numpy as np
+            import matplotlib.pyplot as plt
+            
+            from eazy.templates import Redden
+            
+            fig, ax = plt.subplots(1,1,figsize=(6,4))
+            
+            wave = np.arange(1200, 2.e4)
+            
+            for model in ['calzetti00', 'mw', 'smc', 'reddy15']:
+                redfunc = Redden(model=model, Av=1.0)
+                ax.plot(wave, redfunc(wave), label=model)
+            
+            ax.plot(wave, wave*0+10**(-0.4), color='k', 
+                      label=r'$A_\lambda = 1$', linestyle=':')
+            
+            ax.legend()
+            ax.loglog()
+            
+            ax.set_xticks([2000, 5000, 1.e4])
+            ax.set_xticklabels([0.2, 0.5, 1.0])
+            
+            ax.grid()
+            ax.set_xlabel('wavelength, microns')
+            ax.set_ylabel('Attenuation / extinction (linear)\n' + r'$A_V=1$')
+            
+            fig.tight_layout(pad=0.5)
+            
         Parameters
         ----------
         model : `extinction`/`attenuation` object or str
