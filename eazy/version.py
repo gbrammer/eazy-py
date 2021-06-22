08a3884133042ec378fe24ed95a77ffc3e5980ff
Gabriel Brammer <gbrammer@gmail.com>
2021-06-22 13:51:35 +0200

    bump version 0.5

--- eazy/version.py
+++ eazy/version.py
@@ -1,4 +1,5 @@
 # git describe --tags
-__version__ = "0.4.dev103"
-__long_version__ = "0.4-103-g82b1f70"
-__version_hash__ = "g82b1f70"
+__version__ = "0.5"
+__long_version__ = "0.5"
+__version_hash__ = "g0e60fe7"
+    
