d7b435f953c58d0464fe281b0d6bbeb8bacebe1c
Gabriel Brammer <gbrammer@gmail.com>
2022-02-10 21:20:51 +0100

    version

--- eazy/version.py
+++ eazy/version.py
@@ -1,5 +1,5 @@
 # git describe --tags
-__version__ = "0.5.2.dev3"
-__long_version__ = "0.5.2-3-gb82b6dd"
-__version_hash__ = "gb82b6dd"
+__version__ = "0.5.2.dev45"
+__long_version__ = "0.5.2-45-g4cff093"
+__version_hash__ = "g4cff093"
     
